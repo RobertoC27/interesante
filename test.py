2864a5bdbc7b3e0b3d55e7d82c1c8817f54f5337
Unknown <chi13027@uvg.edu.gt>
2017-10-03 18:32:29 -0600

    mlpoe

---
+++ test.py
@@ -0,0 +1,12 @@
+import unittest
+
+class Simple(unittest.TestCase):
+	def setUp(self):
+		print 'hola'
+	def testMe(self):
+		print 'nuay'
+	def test_malo(self):
+		print 'va pasar'
+		#raise ValueError()
+if __name__=="__main__":
+unittest.main()
